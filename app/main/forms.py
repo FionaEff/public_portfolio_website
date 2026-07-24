2f188d150c34832adc9973306247d414c34941ca
FionaEff <137064797+FionaEff@users.noreply.github.com>
2026-07-24 09:09:19 +0200

    fixed emailfield validation issue

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length
 
 
 class ContactForm(FlaskForm):
 
     name = StringField("Name", validators=[DataRequired(), Length(min=2, max=100)])
-    email = EmailField("Email", validators=[DataRequired(), Email(), Length(max=254)])
-    subject = StringField("Subject", validators=[DataRequired(), Length(min=3, max=150)])
-    message = TextAreaField("Message", validators=[DataRequired(), Length(min=10, max=5000)])
+    email = EmailField("Email", validators=[DataRequired(), Length(max=254)])
+    subject = StringField(
+        "Subject", validators=[DataRequired(), Length(min=3, max=150)]
+    )
+    message = TextAreaField(
+        "Message", validators=[DataRequired(), Length(min=10, max=5000)]
+    )
     submit = SubmitField("Send Message")

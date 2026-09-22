fc209baeda53b3abdcd96c1b14cd58a17f8e2026
FionaEff <137064797+FionaEff@users.noreply.github.com>
2026-09-22 10:55:33 +0200

    added email validator to contact form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Email
 
 
 class ContactForm(FlaskForm):
 
     name = StringField("Name", validators=[DataRequired(), Length(min=2, max=100)])
-    email = EmailField("Email", validators=[DataRequired(), Length(max=254)])
+    email = EmailField("Email", validators=[DataRequired(), Email(), Length(max=254)])
     subject = StringField(
         "Subject", validators=[DataRequired(), Length(min=3, max=150)]
     )
